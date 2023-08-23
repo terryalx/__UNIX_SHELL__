92872ca5fe15e16adb43a96c6551e78cc57029e6
setalxswe <setalxswe@gmail.com>
2023-08-23 13:46:52 -0700

    fork __fork__.c LICENSE pid __pid__.c README.md

--- __fork__.c
+++ __fork__.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	pid_t pp;
 
+	printf("Start..\n");
 	printf("before call..\n");
 
 	fork();
 
 	pp = fork();
 
-	if (pp == 0)
+	if (pp == -1)
 	{
 		printf("failed\n");
 		return -1;
 	}
 	printf("success\n");
+
+	if (pp == 0)
+	{
+		sleep(7);
+		printf("Child proccess..\n");
+	}
+	else
+	{
+		printf("Parent proccess..\n");
+	}
 
 	printf("End..\n");
 
